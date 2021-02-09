ac141b47f72609a5bc52485225654a53efa3e6c0
ThiRO85 <thgr85@gmail.com>
2021-02-09 15:19:08 -0200

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
@@ -8,6 +11,9 @@ public class Main {
 
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
+
     }
 }
