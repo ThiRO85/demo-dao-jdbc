1e3dd44a215378da7d83987abc39eb3965d46815
ThiRO85 <thgr85@gmail.com>
2021-02-13 11:55:25 -0200

    DepartmentDao implementation and test

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,12 +16,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println("=== Test 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
         System.out.println("\n=== Test 2: seller findByDepartment ===");
-        Department department = new Department(2, null);
+        Department department = new Department(1, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj : list) {
             System.out.println(obj);
@@ -49,5 +50,7 @@ public class Main {
         int id = sc.nextInt();
         sellerDao.deleteById(id);
         System.out.println("Delete completed!");
+
+        sc.close();
     }
 }
